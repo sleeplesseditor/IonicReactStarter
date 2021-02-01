36f209c93374b55e9b1d41ecc7f74c5cdf7bb7d2
Josh Bryson <josh@bryson.me.uk>
2021-02-01 23:38:47 +0000

    Ionic added to project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@ionic/react/css/core.css'
 
 ReactDOM.render(
   <React.StrictMode>
